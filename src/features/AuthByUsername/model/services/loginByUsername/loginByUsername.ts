08161078788a81856c9e78a4adfce44c0d89795f
Serhii Malakhov <serzhmalakhov02@gmail.com>
2025-08-28 03:29:42 +0200

    test change internal json to mockapi.io

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { User, userActions } from '@/entities/User';
 import { ThunkConfig } from '@/app/providers/StoreProvider';
+import axios from 'axios';
 
 interface LoginByUsernameProps {
     username: string;
@@ -15,13 +16,21 @@ export const loginByUsername = createAsyncThunk<
     const { extra, dispatch, rejectWithValue } = thunkApi;
 
     try {
-        const response = await extra.api.post<User>('/login', authData);
+      
+        // const response = await extra.api.post<User>('/login', authData);
+    const response = await axios.get('https://68afa839b91dfcdd62bcc5e0.mockapi.io/users');
+    
+    const allUsers = response.data; // Получаем массив всех пользователей
 
+    // ШАГ 2: Ищем в полученном массиве нашего пользователя
+    const foundUser: User = allUsers.find(
+      (user:User) => user.username === authData.username
+    );
         if (!response.data) {
             throw new Error();
         }
-
-        dispatch(userActions.setAuthData(response.data));
+dispatch(userActions.setAuthData(foundUser));
+        // dispatch(userActions.setAuthData(response.data));
         return response.data;
     } catch (e) {
         console.log(e);
